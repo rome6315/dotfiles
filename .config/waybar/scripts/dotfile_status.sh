73226e8b330fdf1d3efe19145ed24b48909379c6
rome6315 <romo6315@gmail.com>
2025-01-20 10:39:19 -0500

    change dotfile status script to work with new repo

--- .config/waybar/scripts/dotfile_status.sh
+++ .config/waybar/scripts/dotfile_status.sh
@@ -2,20 +2,24 @@
 
 
 # Set the path to your Git repository
-REPO_PATH="$HOME/.config"
+REPO_PATH="$HOME/dotfiles"
+
+
+cd "$REPO_PATH"
 
 # Get the current branch
-branch=$(git --git-dir=$REPO_PATH symbolic-ref --short HEAD 2>/dev/null)
+branch=$(git symbolic-ref --short HEAD 2>/dev/null)
+
 
 # ignore these files since all they do is change colors. no need to constantly push color changes
 IGNORE_FILES='waybar/style.css|cava/config|dunst/dunstrc|hypr/scripts/set_wallpaper.sh|hypr/theme.conf|kitty/current-theme.conf|kitty/kitty.conf|kitty/kitty.conf.bak|nwg-dock-hyprland/theme.css|rofi/config.rasi|starship/starship.toml'
 
-# Check for staged, and unstaged changes
-staged=$(git --git-dir=$REPO_PATH diff --cached --numstat | wc -l)
 
-# Run the git diff command and exclude the files using the variable
-unstaged=$(git --git-dir=$REPO_PATH --work-tree "$HOME" diff --numstat | \
-  grep -v -E "$IGNORE_FILES" | wc -l)
+# Check for staged changes
+staged=$(git diff --cached --numstat | wc -l)
+
+# Check for unstaged changes (excluding ignored files)
+unstaged=$(git diff --numstat | grep -v -E "$IGNORE_FILES" | wc -l)
 
 # Build the status string
 status=""
