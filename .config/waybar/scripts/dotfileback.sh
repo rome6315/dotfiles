c7ebe004a007b170039dbb451bff58929e9c06af
rome6315 <romo6315@gmail.com>
2025-01-21 14:58:04 -0500

    change waybar dotfile script

--- .config/waybar/scripts/dotfileback.sh
+++ .config/waybar/scripts/dotfileback.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+
 # Set the path to your Git repository
 REPO_PATH="$HOME/dotfiles"
 
+
 cd "$REPO_PATH"
 
-# Ignore these files since all they do is change colors. no need to constantly push color changes
-IGNORE_FILES='waybar/style.css|cava/config|dunst/dunstrc|hypr/scripts/set_wallpaper.sh|hypr/theme.conf|kitty/current-theme.conf|kitty/kitty.conf|kitty/kitty.conf.bak|nwg-dock-hyprland/theme.css|rofi/config.rasi|starship/starship.toml|scripts/theme-switcher/current_theme.txt'
+# Get the current branch
+branch=$(git --git-dir=$REPO_PATH symbolic-ref --short HEAD 2>/dev/null)
+
+# ignore these files since all they do is change colors. no need to constantly push color changes
+IGNORE_FILES='waybar/style.css|cava/config|dunst/dunstrc|hypr/scripts/set_wallpaper.sh|hypr/theme.conf|kitty/current-theme.conf|kitty/kitty.conf|kitty/kitty.conf.bak|nwg-dock-hyprland/theme.css|rofi/config.rasi|starship/starship.toml'
+
 
 # Check for staged changes
 staged=$(git diff --cached --numstat | wc -l)
 
 # Check for unstaged changes (excluding ignored files)
 unstaged=$(git diff --numstat | grep -v -E "$IGNORE_FILES" | wc -l)
-
-# Get the list of files with unstaged changes (excluding ignored files)
-unstaged_files=($(git diff --name-only | grep -v -E "$IGNORE_FILES"))
 
 # Build the status string
 status=""
@@ -33,15 +36,6 @@ if [[ -z $status ]]; then
   status=" Clean"
 fi
 
-# Build tooltip content with newline-separated files
-tooltip="Files to Push:"
-for file in "${unstaged_files[@]}"; do
-  tooltip+="\n$file"
-done
-
-# Escape double quotes in JSON
-tooltip=$(echo "$tooltip" | sed 's/"/\\"/g')
-
 # Output for Waybar
-echo "{\"text\": \"$status\", \"tooltip\": \"$tooltip\"}"
+echo "{\"text\": \"$status\", \"tooltip\": \"Git Repository: $REPO_PATH - Branch: $branch\"}"
 
