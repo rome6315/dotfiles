188dcf0239c56e63c313e72052103504840e9d3f
rome6315 <romo6315@gmail.com>
2026-03-27 19:18:58 -0400

    swww renamed to awww

--- .config/scripts/theme-switcher/wallpaper_theme.sh
+++ .config/scripts/theme-switcher/wallpaper_theme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-swww img -o DP-1  --transition-type grow --transition-fps 255 ~/Pictures/Wallpapers/$1 #set wallpaper 1
-swww img -o DP-2  --transition-type grow --transition-fps 255 ~/Pictures/Wallpapers/$1 #set wallpaper 2
+awww img -o DP-1  --transition-type grow --transition-fps 255 ~/Pictures/Wallpapers/$1 #set wallpaper 1
+awww img -o DP-2  --transition-type grow --transition-fps 255 ~/Pictures/Wallpapers/$1 #set wallpaper 2
 
