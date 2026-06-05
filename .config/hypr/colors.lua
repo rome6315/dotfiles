7401fcb79baed95b6bd409ae4157640bbd2a0d34
Roman <romo6315@gmail.com>
2026-06-05 19:13:42 -0400

    update waybar and scripts

--- .config/hypr/colors.lua
+++ .config/hypr/colors.lua
@@ -1,104 +1,104 @@
 return {
-    image = "/home/roman/Pictures/Matugen/dark-city-overlook.png",
+    image = "/home/roman/Pictures/Matugen/rivendale.jpg",
 
-    background = "0xff141318",
+    background = "0xff17130b",
 
     error = "0xffffb4ab",
 
     error_container = "0xff93000a",
 
-    inverse_on_surface = "0xff313036",
+    inverse_on_surface = "0xff353027",
 
-    inverse_primary = "0xff5e5791",
+    inverse_primary = "0xff79590c",
 
-    inverse_surface = "0xffe5e1e9",
+    inverse_surface = "0xffebe1d4",
 
-    on_background = "0xffe5e1e9",
+    on_background = "0xffebe1d4",
 
     on_error = "0xff690005",
 
     on_error_container = "0xffffdad6",
 
-    on_primary = "0xff30285f",
+    on_primary = "0xff402d00",
 
-    on_primary_container = "0xffe5deff",
+    on_primary_container = "0xffffdea3",
 
-    on_primary_fixed = "0xff1a1249",
+    on_primary_fixed = "0xff261900",
 
-    on_primary_fixed_variant = "0xff463f77",
+    on_primary_fixed_variant = "0xff5c4200",
 
-    on_secondary = "0xff312e41",
+    on_secondary = "0xff3b2f15",
 
-    on_secondary_container = "0xffe5dff9",
+    on_secondary_container = "0xfff6e0bb",
 
-    on_secondary_fixed = "0xff1c192b",
+    on_secondary_fixed = "0xff251a04",
 
-    on_secondary_fixed_variant = "0xff474459",
+    on_secondary_fixed_variant = "0xff53452a",
 
-    on_surface = "0xffe5e1e9",
+    on_surface = "0xffebe1d4",
 
-    on_surface_variant = "0xffc9c5d0",
+    on_surface_variant = "0xffd1c5b4",
 
-    on_tertiary = "0xff482536",
+    on_tertiary = "0xff1e361b",
 
-    on_tertiary_container = "0xffffd8e7",
+    on_tertiary_container = "0xffcdebc3",
 
-    on_tertiary_fixed = "0xff301121",
+    on_tertiary_fixed = "0xff092008",
 
-    on_tertiary_fixed_variant = "0xff613b4d",
+    on_tertiary_fixed_variant = "0xff344d2f",
 
-    outline = "0xff928f99",
+    outline = "0xff9a8f80",
 
-    outline_variant = "0xff48454f",
+    outline_variant = "0xff4d4639",
 
-    primary = "0xffc8bfff",
+    primary = "0xffebc16c",
 
-    primary_container = "0xff463f77",
+    primary_container = "0xff5c4200",
 
-    primary_fixed = "0xffe5deff",
+    primary_fixed = "0xffffdea3",
 
-    primary_fixed_dim = "0xffc8bfff",
+    primary_fixed_dim = "0xffebc16c",
 
     scrim = "0xff000000",
 
-    secondary = "0xffc9c3dc",
+    secondary = "0xffd9c4a0",
 
-    secondary_container = "0xff474459",
+    secondary_container = "0xff53452a",
 
-    secondary_fixed = "0xffe5dff9",
+    secondary_fixed = "0xfff6e0bb",
 
-    secondary_fixed_dim = "0xffc9c3dc",
+    secondary_fixed_dim = "0xffd9c4a0",
 
     shadow = "0xff000000",
 
-    source_color = "0xffcbc4f0",
+    source_color = "0xffa4906c",
 
-    surface = "0xff141318",
+    surface = "0xff17130b",
 
-    surface_bright = "0xff3a383e",
+    surface_bright = "0xff3e382f",
 
-    surface_container = "0xff201f25",
+    surface_container = "0xff241f17",
 
-    surface_container_high = "0xff2a292f",
+    surface_container_high = "0xff2e2921",
 
-    surface_container_highest = "0xff35343a",
+    surface_container_highest = "0xff39342b",
 
-    surface_container_low = "0xff1c1b20",
+    surface_container_low = "0xff1f1b13",
 
-    surface_container_lowest = "0xff0e0e13",
+    surface_container_lowest = "0xff120e07",
 
-    surface_dim = "0xff141318",
+    surface_dim = "0xff17130b",
 
-    surface_tint = "0xffc8bfff",
+    surface_tint = "0xffebc16c",
 
-    surface_variant = "0xff48454f",
+    surface_variant = "0xff4d4639",
 
-    tertiary = "0xffecb8ce",
+    tertiary = "0xffb1cfa8",
 
-    tertiary_container = "0xff613b4d",
+    tertiary_container = "0xff344d2f",
 
-    tertiary_fixed = "0xffffd8e7",
+    tertiary_fixed = "0xffcdebc3",
 
-    tertiary_fixed_dim = "0xffecb8ce",
+    tertiary_fixed_dim = "0xffb1cfa8",
 
 }
--- .config/waybar/scripts/dotfile_status.sh
+++ .config/waybar/scripts/dotfile_status.sh
@@ -5,43 +5,32 @@ REPO_PATH="$HOME/dotfiles"
 
 cd "$REPO_PATH"
 
-# Ignore these files since all they do is change colors. no need to constantly push color changes
-IGNORE_FILES='waybar/style.css|cava/config|dunst/dunstrc|hypr/scripts/set_wallpaper.sh|hypr/theme.conf|kitty/current-theme.conf|kitty/kitty.conf|kitty/kitty.conf.bak|nwg-dock-hyprland/theme.css|rofi/config.rasi|starship/starship.toml|scripts/theme-switcher/current_theme.txt'
+# ignore these files since all they do is change colors
+#IGNORE_FILES='waybar/style.css|cava/config|dunst/dunstrc|hypr/scripts/set_wallpaper.sh|hypr/theme.conf|kitty/current-theme.conf|kitty/kitty.conf|kitty/kitty.conf.bak|nwg-dock-hyprland/theme.css|rofi/config.rasi|starship/starship.toml|scripts/theme-switcher/current_theme.txt'
+IGNORE_FILES='dunstrc|starship.toml|kitty/themes/Matugen.conf|kitty/current-theme.conf|colors\.[a-z]+|\.uuid$'
 
-# Check for staged changes
-staged=$(git diff --cached --numstat | wc -l)
-
-# Check for unstaged changes (excluding ignored files)
+# get number of unstaged changes (excluding ignored files)
 unstaged=$(git diff --numstat | grep -v -E "$IGNORE_FILES" | wc -l)
 
-# Get the list of files with unstaged changes (excluding ignored files)
+# get the list of files with unstaged changes (excluding ignored files)
 unstaged_files=($(git diff --name-only | grep -v -E "$IGNORE_FILES"))
 
-# Build the status string
-status=""
-
-if [[ $unstaged -gt 0 && $staged -gt 0 ]]; then
-  status+=" $unstaged unstaged  $staged staged" # If both staged and unstaged changes, only display each once
-elif [[ $unstaged -gt 0 && $staged -eq 0 ]]; then
-  status+=" $unstaged unstaged" # If only unstaged changes, don't display staged
-elif [[ $unstaged -eq 0 && $staged -gt 0 ]]; then
-  status+=" $staged staged" # If only staged changes, don't display unstaged
+# set status var which will be used to display in waybar
+if [[ $unstaged -gt 0 ]]; then
+  status="$unstaged"
+else
+  status="Clean"
 fi
 
-# Default message if everything is clean
-if [[ -z $status ]]; then
-  status=" Clean"
-fi
-
-# Build tooltip content with newline-separated files
+# tooltip to display which files were changed
 tooltip="Files to Push:"
 for file in "${unstaged_files[@]}"; do
   tooltip+="\n$file"
 done
 
-# Escape double quotes in JSON
+# escape double quotes in JSON
 tooltip=$(echo "$tooltip" | sed 's/"/\\"/g')
 
-# Output for Waybar
+# output for Waybar
 echo "{\"text\": \"$status\", \"tooltip\": \"$tooltip\"}"
 
