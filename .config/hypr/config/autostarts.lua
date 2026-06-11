aefb4679db254d2235014f61e063e6d8325a7b3f
Roman <romo6315@gmail.com>
2026-06-10 20:37:14 -0400

    update packages list

--- .config/hypr/config/autostarts.lua
+++ .config/hypr/config/autostarts.lua
@@ -2,7 +2,7 @@
 hl.on("hyprland.start", function()
     hl.exec_cmd("hypridle")
     hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
-    hl.exec_cmd("/usr/lib/polkit-kde-authentication-agent-1")
+    hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
     hl.exec_cmd("dunst")
     hl.exec_cmd("waybar")
     hl.exec_cmd("awww-daemon")
